75684120eab7ea0e7e6192846a591be546e07e43
jakobpalmborg <jakob.palmborg@gmail.com>
2023-11-07 09:31:10 +0100

    getMapping for categories

---
+++ src/main/java/com/example/geoapi/Controller.java
@@ -0,0 +1,19 @@
+package com.example.geoapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/categories")
+    public ResponseEntity<String> getAllCategories() {
+        return new ResponseEntity<String>("This is all categories", HttpStatus.OK);
+    }
+
+}
