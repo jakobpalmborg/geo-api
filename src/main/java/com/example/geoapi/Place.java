f3867518a64e8056434c864865c4f55add504baa
jakobpalmborg <jakob.palmborg@gmail.com>
2023-11-13 15:19:35 +0100

    add entity User and entity Place

---
+++ src/main/java/com/example/geoapi/Place.java
@@ -0,0 +1,113 @@
+package com.example.geoapi;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "place", schema = "geographically")
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @Column(name = "private")
+    private Boolean privateField;
+
+    @Column(name = "time_modified")
+    private Instant timeModified;
+
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "time_created")
+    private Instant timeCreated;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Boolean getPrivateField() {
+        return privateField;
+    }
+
+    public void setPrivateField(Boolean privateField) {
+        this.privateField = privateField;
+    }
+
+    public Instant getTimeModified() {
+        return timeModified;
+    }
+
+    public void setTimeModified(Instant timeModified) {
+        this.timeModified = timeModified;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Instant timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+/*
+    TODO [JPA Buddy] create field to map the 'coordinates' column
+     Available actions: Define target Java type | Uncomment as is | Remove column mapping
+    @Column(name = "coordinates", columnDefinition = "point(0, 0)")
+    private Object coordinates;
+*/
+}
---
+++ src/main/java/com/example/geoapi/User.java
@@ -0,0 +1,33 @@
+package com.example.geoapi;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @Column(name = "user_name")
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
