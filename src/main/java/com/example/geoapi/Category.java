8b8951a9a298a788261e48d6f6e1879aba216a50
jakobpalmborg <jakob.palmborg@gmail.com>
2023-11-13 13:31:34 +0100

    add entity Category

---
+++ src/main/java/com/example/geoapi/Category.java
@@ -0,0 +1,48 @@
+package com.example.geoapi;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Category {
+    @Id
+    private Integer id;
+
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    @Size(max = 255)
+    @Column(name = "symbol")
+    private String symbol;
+
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
